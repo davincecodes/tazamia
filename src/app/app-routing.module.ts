4ffb0c85a8ab04ca3ee47924f5138f2eede8aa4c
Vincent Ramirez <vincentramirez@Vincents-MacBook-Pro.local>
2019-03-14 21:02:50 +0800

    Generate routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./_views/home/home.component";
+import { AboutComponent } from "./_views/about/about.component";
+import { MenuComponent } from "./_views/menu/menu.component";
+import { ContactComponent } from "./_views/contact/contact.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  {
+    path: "menu",
+    component: MenuComponent
+  },
+  {
+    path: "contact",
+    component: ContactComponent
+  },
+  {
+    path: "**",
+    redirectTo: ""
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
